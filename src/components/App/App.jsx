80c14cc79e483fd8868a10d37e5726d8cc8a4c80
Kristina <ktuzova1901@gmail.com>
2022-05-10 02:26:28 +0300

    state

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,15 +9,22 @@ import s from '../App/App.module.css';
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
+    name: '',
+    number: '',
   };
 
   repeatCheck = newName => {
     return this.state.contacts.find(({ name }) => name === newName);
   };
   showNotification = name => {
-    alert(`${name} is already contacts`);
+    alert(`${name} is already in contacts`);
   };
   addContact = ({ name, number }) => {
     if (!this.repeatCheck(name)) {
@@ -79,7 +86,7 @@ class App extends Component {
               onDeleteContact={this.deleteContact}
             />
           ) : (
-            <p className={s.text}>There’s nothing here yet...</p>
+            <p></p>
           )}
         </Section>
       </>
